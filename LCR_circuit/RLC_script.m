efe79c929a6f3e786c9e89610930377d9d00b417
jeg114 <jeg114@ic.ac.uk>
2016-02-28 19:13:58 +0000

    Format Ruka not finished

--- LCR_circuit/RLC_script.m
+++ LCR_circuit/RLC_script.m
@@ -1,37 +1,59 @@
+%% RUNGE-Kutta_SCRIPT PLOT
+%function rukasecond_script()
+
+%%CONDITIONS
 R=250;
 L=600e-3;
 C=3.5e-6;
-
 f=109;
-
-step=@(t) 5*heaviside(t);
-impulsive_decay=@(t) step(t)*exp(-t/3e-3);
-sin_f=@(t) sin(2*pi*f*t);
-sqr_f=@(t) heaviside(sin(2*pi*f*t));
-
-v_in=step;
-
 h=0.0000008;
 N=100000;
+q(1)=500e-9;
+i(1)=0;
+
+
+%%
+%STEP SIGNAL
+step=@(t) 5*heaviside(t);
+%IMPULSIVE SIGNAL WITH DECAY
+impulsive_decay=@(t) step(t)*exp(-t/3e-3);
+%SQUARE WAVE
+sin_f=@(t) sin(2*pi*f*t);
+%SINE WAVE
+sqr_f=@(t) heaviside(sin(2*pi*f*t));
+
+v_in=step;          %Assign one of above signals to v_in
+
 q=zeros(N,1);
 i=zeros(N,1);
 in=zeros(N,1);
-q(1)=500e-9;
-i(1)=0;
-%t=zeros(500,1);
-t=(0:h:h*(N-1));
-t=t';
-func=@(q, i, t) (1/L)*(v_in(t)-(R*i)-(q/C));
 
+%t=zeros(500,1);
+t=(0:h:h*(N-1));    %Vector from 0 to h*(N-1) in in increments of h
+t=t';               %Transpose vector
+
+
+%CLASSIC FOURTH-ORDER RUNGE-KUTTA ALGORITHM 
+func=@(q, i, t) (1/L)*(v_in(t)-(R*i)-(q/C)); 
 for ind = 1:N-1
    [q(ind+1), i(ind+1)]=rukasecond(q(ind), i(ind), t(ind), h, func); 
    in(ind)=v_in(t(ind));
 end
+
 %plot(sin(2*pi*109*t));
+%VOLTAGE PLOT
 subplot(3, 1, 1);
 plot(t, q);
+title('Graph of Vout against time of Signal');
+xlabel('Time (s)');
+ylabel('Vout (V)');
+%CURRENT PLOT
 subplot(3, 1, 2);
 plot(t, i);
+title('Graph of Iout against time of Signal');
+xlabel('Time (s)');
+ylabel('Iout (A)');
+%INPUT SIGNAL PLOT
 subplot(3, 1, 3);
 plot(t, in);
 % plot(t, q, t, i);
--- LCR_circuit/rukasecond.m
+++ LCR_circuit/rukasecond.m
@@ -1,6 +1,41 @@
+%% RUKASECOND METHOD
+% RK_4_2 Step of 4th order Runge-Kutta algorithm for second order ODE
+%
+% We need to implement two linked Runge-Kutta algorithms for
+%
+% $$ q'=i \hspace{1.65in} equation \hspace{0.03in} 1 $$
+% 
+% $$ ai'+bi+cq=d \hspace{1in} equation \hspace{0.03in} 2 $$
+% 
+% which were obtained from the second order ODE:  
+%
+% $$ aq''+bq'+cq=d $$
+%
+%
+% The function "func" is obtained from 2) by rearranging:
+%
+% $$ i'=(1/a)*(d-bi-cq) $$
+%
+% and replacing a, b, c and d with L, v_in, R and C respectively. 
+%
+%
+% In our code we used x = q and y = i, so that the algorithm can be 
+% used to solve other problems.
+%
+%
+% The coefficients for the Runge-Kutta algorithms for
+%
+% $$ k_{x1} $$ , $$ k_{x2} $$ , $$ k_{x3} $$ and $$ k_{x4} \hspace{1in} for \hspace{0.03in} equation  \hspace{0.03in} 1 $$
+%
+% $$ k_{y1} $$ , $$ k_{y2} $$ , $$ k_{y3} $$ and $$ k_{y4} \hspace{1in} for \hspace{0.03in} equation \hspace{0.03in} 2 $$ 
+%
+%
+% Note that for optimizing the code, instead of calling the function
+% "func" for 1), we call:
+%
+% $$ y_{i} = y + h * k_{y(i-1)} $$
+%%
 function [ x_it,y_it ] = rukasecond( x, y, t, h, func)
-%RK_4_2 Step of 4th order Runge-Kutta algorithm for second order ODE
-%   Detailed explanation goes here
     k_y1=func(x, y, t);
     k_x1=y;
     k_y2=func(x+0.5*h*k_x1, y+0.5*h*k_y1, t+0.5*h);
@@ -12,5 +47,7 @@ function [ x_it,y_it ] = rukasecond( x, y, t, h, func)
     
     x_it=x+h*(k_x1+2*(k_x2+k_x3)+k_x4)/6;
     y_it=y+h*(k_y1+2*(k_y2+k_y3)+k_y4)/6;
+
+
 end
 
